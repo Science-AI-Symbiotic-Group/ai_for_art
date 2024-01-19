63b84f8e92e2e55f52033bd6ec2d93140b45e462
CorruptedFile <lyricalmusicvideos2607@gmail.com>
2024-01-19 18:15:30 +0530

    added option to add a background video, change video to process and the gamma of the background video as command line arguments using argparse

--- contour_art_opencv.py
+++ contour_art_opencv.py
@@ -1,9 +1,50 @@
 import cv2
 import numpy as np
+import argparse
+import os
 
-cap = cv2.VideoCapture('videos/6.mp4')   #replace with webcam number or file name
-cap_bg = cv2.VideoCapture('background/bg_1.mp4')   #replace with background file name
-#frame_bg = cv2.imread('background/2.jpg')   #replace with background file name for image
+argument_parser = argparse.ArgumentParser()
+
+argument_parser.add_argument("video")
+argument_parser.add_argument("--background_video",help="Select the Video you want for the Background. Must be a Video File")
+argument_parser.add_argument("--background_gamma",help="The Value for the Gamma of the Background Video, By Default is 1.0")
+
+
+arguments = argument_parser.parse_args()
+
+using_bg = True
+
+if arguments.background_gamma == None:
+    background_gamma = 1.0  #default value for gamma
+else:
+    background_gamma = float(arguments.background_gamma)
+
+
+
+
+if arguments.background_video == None:
+    using_bg = False
+    print("Not Using a Background Video")
+else:
+    if os.path.exists(arguments.background_video) == False:
+        print(f"The Background Video does not exist")
+        raise SystemExit(1)
+    else:
+        using_bg = True
+
+
+if os.path.exists(arguments.video) == False:
+    print("The video does not exist")
+    raise SystemExit(1)
+
+
+
+
+
+cap = cv2.VideoCapture(arguments.video)   #replace with webcam number or file name
+
+if using_bg == True:
+    cap_bg = cv2.VideoCapture(arguments.background_video)   #replace with background file name
 
 # Get the video details (width, height, frames per second)
 width = cap.get(3)
@@ -35,7 +76,9 @@ def adjust_gamma(image, gamma=1.0):
 while True:
     # Read a frame from the webcam
     ret, frame = cap.read()
-    ret_bg, frame_bg = cap_bg.read()
+
+    if using_bg == True:
+        ret_bg, frame_bg = cap_bg.read()
     
 
     # Check if the frame is read successfully
@@ -45,15 +88,14 @@ while True:
 
 
     try:
-        frame_bg = cv2.resize(frame_bg, (width_int,height_int))
-        
-
-        frame_bg = adjust_gamma(frame_bg,1.0)
+        if using_bg == True:
+            frame_bg = cv2.resize(frame_bg, (width_int,height_int))        
+            frame_bg = adjust_gamma(frame_bg,1.0)
 
         # Convert the frame to grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray=cv2.medianBlur(gray,1)
-        gray = cv2.GaussianBlur(src=gray, ksize=(3, 5), sigmaX=10) 
+        gray = cv2.GaussianBlur(src=gray, ksize=(3, 5), sigmaX=100) 
         # Adjust Canny edge detection thresholds for sensitivity
         low_threshold = 70 # You can experiment with different values
         high_threshold = 150  # You can experiment with different values
@@ -70,9 +112,8 @@ while True:
         contour_frame = black_background.copy()
         cv2.drawContours(contour_frame, contours, -1, (0, 255, 0), 1)
         
-        
-        contour_frame = cv2.add(contour_frame,frame_bg)
-        
+        if using_bg == True:
+            contour_frame = cv2.add(contour_frame,frame_bg)
         
         out.write(contour_frame)
 
