9789ce78462db45b5a6be12c160884016c8df25c
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2024-01-23 21:10:50 +0530

    Update mediapipe_pose_detector_lite.py

--- mediapipe_try/mediapipe_pose_detector_lite.py
+++ mediapipe_try/mediapipe_pose_detector_lite.py
@@ -6,50 +6,44 @@ import numpy as np
 ## initialize pose estimator
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
-
-
-mp_styles = mp.solutions.drawing_styles
-
-landmark_annotations = mp_styles.get_default_pose_landmarks_style()
-
-custom_color_1 = (121, 111, 22)
-landmark_annotations= mp_drawing.DrawingSpec(
-    color=custom_color_1, thickness=4, circle_radius=10)
-
-
-
-
 pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
+cap = cv2.VideoCapture('video4.mp4')
+# Get the video details (width, height, frames per second)
 
-cap = cv2.VideoCapture(1)
+
+
+# Define the codec and create a VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output_video4.avi', fourcc, 25.0, (608, 1080))
 
 
 while cap.isOpened():
-    # read fram
-     _, frame = cap.read()
+    # read frame
+    _, frame = cap.read()
+    try:
+         # resize the frame for portrait video
+         # frame = cv2.resize(frame, (350, 600))
+         # convert to RGB
+         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+         
+         # process the frame for pose detection
+         pose_results = pose.process(frame_rgb)
 
-     # resize the frame for portrait video
-     # frame = cv2.resize(frame, (350, 600))
-     # convert to RGB
-     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+         black_background=np.zeros_like(frame)
+         # print(pose_results.pose_landmarks)
+         
+         # draw skeleton on the frame
+         mp_drawing.draw_landmarks(black_background, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+         # display the frame
+         cv2.imshow('Output', black_background)
+         out.write(black_background)
 
-     # process the frame for pose detection
-     pose_results = pose.process(frame_rgb)
-     # print(pose_results.pose_landmarks)
-     black_bkgrnd=np.zeros_like(frame_rgb)
-
-     # draw skeleton on the frame
-
-     mp_drawing.draw_landmarks(black_bkgrnd, pose_results.pose_landmarks, 
-                              mp_pose.POSE_CONNECTIONS,
-                              landmark_drawing_spec=landmark_annotations)
-     # display the frame
-     cv2.imshow('Output', black_bkgrnd)
-     
-     
-     if cv2.waitKey(1) == ord('q'):
+    except:
+         break
+    
+    if cv2.waitKey(1) == ord('q'):
           break
-               
+          
 cap.release()
 cv2.destroyAllWindows()
